50ccb34a97216c9f842b514cdafb7d36e935faf0
Alexandr Skaba <95905498+parsonsoff@users.noreply.github.com>
2024-05-20 00:28:34 +0300

    lab4

---
+++ lab4/receiver.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+    // Открываем общий файл для чтения
+    HANDLE file = OpenFileMapping(FILE_MAP_READ, FALSE, L"shared_file");
+    if (file == NULL) {
+        std::cerr << "Failed to open shared file." << std::endl;
+        return 1;
+    }
+
+    // Открываем семафор
+    HANDLE semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"message_sem");
+    if (semaphore == NULL) {
+        std::cerr << "Failed to open semaphore." << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        // Ждем, пока семафор не станет равным 1
+        WaitForSingleObject(semaphore, INFINITE);
+
+        // Читаем сообщение из файла
+        LPCTSTR message = (LPCTSTR)MapViewOfFile(file, FILE_MAP_READ, 0, 0, 0);
+        std::wstring wideMessage(message);
+        int size = WideCharToMultiByte(CP_UTF8, 0, wideMessage.c_str(), -1, NULL, 0, NULL, NULL);
+        std::string messageStr(size, '\0');
+        WideCharToMultiByte(CP_UTF8, 0, wideMessage.c_str(), -1, &messageStr[0], size, NULL, NULL);
+        UnmapViewOfFile(message);
+
+        // Выводим сообщение на экран
+        std::cout << "Received message: " << messageStr << std::endl;
+
+        // Устанавливаем значение семафора обратно в 0
+        ReleaseSemaphore(semaphore, 1, NULL);
+    }
+
+    // Закрываем файл и семафор
+    CloseHandle(file);
+    CloseHandle(semaphore);
+
+    return 0;
+}
---
+++ lab4/sender.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+    // Создаем или открываем общий файл для записи
+    HANDLE file = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 256, L"shared_file");
+    if (file == NULL) {
+        std::cerr << "Failed to create or open shared file." << std::endl;
+        return 1;
+    }
+
+    // Создаем или открываем семафор
+    HANDLE semaphore = CreateSemaphore(NULL, 0, 1, L"message_sem");
+    if (semaphore == NULL) {
+        std::cerr << "Failed to create or open semaphore." << std::endl;
+        return 1;
+    }
+
+    while (true) {
+        // Читаем сообщение с клавиатуры
+        std::string message;
+        std::cout << "Enter message: ";
+        std::getline(std::cin, message);
+
+        // Записываем сообщение в файл
+        std::wstring wideMessage(message.begin(), message.end());
+        LPVOID messageBuffer = MapViewOfFile(file, FILE_MAP_WRITE, 0, 0, 0);
+        std::copy(wideMessage.c_str(), wideMessage.c_str() + wideMessage.size() + 1, (WCHAR*)messageBuffer);
+        UnmapViewOfFile(messageBuffer);
+
+        // Устанавливаем значение семафора в 1
+        ReleaseSemaphore(semaphore, 1, NULL);
+    }
+
+    // Закрываем файл и семафор
+    CloseHandle(file);
+    CloseHandle(semaphore);
+
+    return 0;
+}
